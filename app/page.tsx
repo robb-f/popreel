1d0d30f9dc2e5c92aaea7b7abbe7875d944c467d
Robert Fornos <rf2830@columbia.edu>
2025-01-26 18:06:06 -0500

    Remove logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,9 @@
 import { SignInButton, SignedIn, SignedOut } from "@clerk/nextjs"
-import { LearnMore } from "./components/learn-more"
-// import screenshotDevices from "./images/user-button@2xrl.webp"
-// import signIn from "./images/sign-in@2xrl.webp"
-// import verify from "./images/verify@2xrl.webp"
-// import userButton2 from "./images/user-button-2@2xrl.webp"
 import signUp from "./images/sign-up@2xrl.webp"
 import logo from "./images/logo.png"
 import "./home.css"
 import Image from "next/image"
 import Link from "next/link"
-import { Footer } from "./components/footer"
 
 import { ClerkLogo } from "./components/clerk-logo"
 import { NextLogo } from "./components/next-logo"
@@ -20,12 +14,6 @@ export default function Home() {
       <main className="bg-[#FAFAFA] relative">
         <div className="w-full bg-white max-w-[75rem] mx-auto flex flex-col border-l border-r border-[#F2F2F2] row-span-3">
           <div className="absolute top-0 bottom-0 left-1/2 -translate-x-1/2 w-px bg-[#F2F2F2]" />
-          <Image
-            alt="Device"
-            className="size-64 bg-transparent absolute left-1/2 -translate-x-[23.75rem] -top-6 h-[51.375rem] object-contain w-[39.0625rem]"
-            src={logo}
-            unoptimized
-          />
 
           <div className="px-12 py-16 border-b border-[#F2F2F4]">
             <div className="bg-[#F4F4F5] px-4 py-3 rounded-full inline-flex gap-4">
