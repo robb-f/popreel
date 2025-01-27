cd2835d6655d880c60cd62aba169a35049033912
Robert Fornos <rf2830@columbia.edu>
2025-01-26 22:06:41 -0500

    profile pic line

--- app/components/user-details.tsx
+++ app/components/user-details.tsx
@@ -66,12 +66,6 @@ export function UserDetails() {
         <div className="flex flex-col items-center gap-2 mb-6">
           <div className="w-full relative flex justify-center">
             <img src={user.imageUrl} className="size-20 rounded-full" />
-            <div className="absolute w-fit flex items-center gap-5 top-1/2 -translate-x-2.5 -translate-y-1/2 left-full">
-              <div className="relative">
-                <div className="h-px bg-[#BFBFC4] w-[6.5rem]" />
-                <div className="size-1 bg-[#BFBFC4] rotate-45 absolute right-0 top-1/2 -translate-y-1/2" />
-              </div>
-            </div>
           </div>
           {user.firstName && user.lastName ? (
             <h1 className="text-[1.0625rem] font-semibold relative w-full text-center">
@@ -80,12 +74,6 @@ export function UserDetails() {
                 <div className="relative">
                   <div className="h-px bg-[#BFBFC4] w-[6.5rem]" />
                   <div className="size-1 bg-[#BFBFC4] rotate-45 absolute right-0 top-1/2 -translate-y-1/2" />
-                </div>
-                <div className="font-mono text-xs bg-black px-1.5 py-1 rounded-md text-white">
-                  user.firstName
-                </div>
-                <div className="font-mono text-xs bg-black px-1.5 py-1 rounded-md text-white -translate-x-3">
-                  user.lastName
                 </div>
               </div>
             </h1>
