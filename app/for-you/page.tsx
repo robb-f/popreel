e4b4789781e66573ae49440ce1c4d365b8094e9d
Robert Fornos <rf2830@columbia.edu>
2025-01-26 23:08:39 -0500

    add for you

---
+++ app/for-you/page.tsx
@@ -0,0 +1,53 @@
+import { UserDetails } from "../components/user-details";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import { CodeSwitcher } from "../components/code-switcher";
+import { LearnMore } from "../components/learn-more";
+import { Footer } from "../components/footer";
+import { ClerkLogo } from "../components/clerk-logo";
+import { NextLogo } from "../components/next-logo";
+import Link from "next/link"
+
+import { DASHBOARD_CARDS } from "../consts/cards";
+
+export default async function DashboardPage() {
+  return (
+    <>
+      <main className="max-w-[75rem] w-full mx-auto">
+          <div>
+            <header className="flex items-center justify-between w-full h-16 gap-4">
+              <div className="flex gap-4">
+                <ClerkLogo />
+                <div aria-hidden className="w-px h-6 bg-[#C7C7C8]" />
+                <NextLogo />
+                <div aria-hidden className="w-px h-6 bg-[#C7C7C8]" />
+                <Link
+                  href="/for-you"
+                  className="px-4 py-2 rounded-full bg-[#131316] text-white text-sm font-semibold"
+                >
+                  For You
+                </Link>
+              </div>
+              <div className="flex items-center gap-2">
+                <OrganizationSwitcher
+                  appearance={{
+                    elements: {
+                      organizationPreviewAvatarBox: "size-6",
+                    },
+                  }}
+                />
+                <UserButton
+                  afterSignOutUrl="/"
+                  appearance={{
+                    elements: {
+                      userButtonAvatarBox: "size-6",
+                    },
+                  }}
+                />
+              </div>
+            </header>
+            <UserDetails />
+          </div>
+      </main>
+    </>
+  );
+}
